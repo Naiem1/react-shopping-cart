29177df875da66b560c67065e80faa2b3a483c3d
Naiem1 <naiem4288@gmail.com>
2024-01-06 22:03:47 +0600

    added product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Header from "./components/header/Header";
-import Hero from "./components/hero-section/Hero";
+import Header from './components/header/Header';
+import Hero from './components/hero-section/Hero';
+import ProductSection from './components/product-section/ProductSection';
 
 const App = () => {
-  return <div>
-
-    <Header />
-    
-
-    <Hero/>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <ProductSection />
+    </div>
+  );
 };
 
 export default App;
